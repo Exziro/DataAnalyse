352dde4cf4d0ceebe85c934bffae55c86590d523
Exziro <qiyixi2010@gmail.com>
2018-01-03 18:28:43 +0800

    label

--- decisiontree.py
+++ decisiontree.py
@@ -90,3 +90,31 @@ def calcShannonEnt(dataSet):
     # shannonEnt = sum([-p * log(p, 2) for p in probs])
     # # -----------计算香农熵的第二种实现方式end--------------------------------------------------------------------------------
     return shannonEnt
+def splitDataSet(dataSet, index, value):
+    """splitDataSet(通过遍历dataSet数据集，求出index对应的colnum列的值为value的行)
+        就是依据index列进行分类，如果index列的数据等于 value的时候，就要将 index 划分到我们创建的新的数据集中
+    Args:
+        dataSet 数据集                 待划分的数据集
+        index 表示每一行的index列        划分数据集的特征
+        value 表示index列对应的value值   需要返回的特征的值。
+    Returns:
+        index列为value的数据集【该数据集需要排除index列】
+    """
+    # -----------切分数据集的第一种方式 start------------------------------------
+    retDataSet = []
+    for featVec in dataSet: 
+        # index列为value的数据集【该数据集需要排除index列】
+        # 判断index列的值是否为value
+        if featVec[index] == value:
+             reducedFeatVec = featVec[:index]
+              reducedFeatVec.extend(featVec[index+1:])
+            # [index+1:]表示从跳过 index 的 index+1行，取接下来的数据
+            # 收集结果值 index列为value的行【该行需要排除index列】
+            retDataSet.append(reducedFeatVec)
+    # -----------切分数据集的第一种方式 end------------------------------------
+
+    # # -----------切分数据集的第二种方式 start------------------------------------
+    # retDataSet = [data for data in dataSet for i, v in enumerate(data) if i == axis and v == value]
+    # # -----------切分数据集的第二种方式 end------------------------------------
+    return retDataSet
+
