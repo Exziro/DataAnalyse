4c0b2fc11721486b20e77ff541f398d987263823
Exziro <qiyixi2010@gmail.com>
2018-01-07 18:31:38 +0800

    label

--- decisiontree.py
+++ decisiontree.py
@@ -182,3 +182,14 @@ def chooseBestFeatureToSplit(dataSet):
     Returns:
         bestFeature 最优的特征列
     """
+    # -----------majorityCnt的第一种方式 start------------------------------------
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    # 倒叙排列classCount得到一个字典集合，然后取出第一个就是结果（yes/no），即出现次数最多的结果
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    # print 'sortedClassCount:', sortedClassCount
+    return sortedClassCount[0][0]
+    # -----------majorityCnt的第一种方式 end------------------------------------
