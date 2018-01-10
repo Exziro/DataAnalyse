abc21cc1d4f962c5f2b9a3309ea8f0e4df8200d9
Exziro <qiyixi2010@gmail.com>
2018-01-10 18:36:28 +0800

    labels

--- decisiontree.py
+++ decisiontree.py
@@ -206,3 +206,16 @@ def chooseBestFeatureToSplit(dataSet):
     # count() 函数是统计括号中的值在list中出现的次数
     if classList.count(classList[0]) == len(classList):
         return classList[0]
+        # 如果数据集只有1列，那么最初出现label次数最多的一类，作为结果
+    # 第二个停止条件：使用完了所有特征，仍然不能将数据集划分成仅包含唯一类别的分组。
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+
+    # 选择最优的列，得到最优列对应的label含义
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    # 获取label的名称
+    bestFeatLabel = labels[bestFeat]
+    # 初始化myTree
+    myTree = {bestFeatLabel: {}}
+    # 注：labels列表是可变对象，在PYTHON函数中作为参数时传址引用，能够被全局修改
+    # 所以这行代码导致函数外的同名变量被删除了元素，造成例句无法执行，提示'no surfacing' is not in list
