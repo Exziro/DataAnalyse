5803a00c33879be96f6cd67bef1a686442852367
Exziro <qiyixi2010@gmail.com>
2018-01-18 18:45:35 +0800

    creat key worlds

--- bayes.py
+++ bayes.py
@@ -21,3 +21,14 @@ def loadDataSet():
                    ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
     classVec = [0, 1, 0, 1, 0, 1]  # 1 is abusive, 0 not
     return postingList, classVec
+def createVocabList(dataSet):
+    """
+    获取所有单词的集合
+    :param dataSet: 数据集
+    :return: 所有单词的集合(即不含重复元素的单词列表)
+    """
+    vocabSet = set([])  # create empty set
+    for document in dataSet:
+        # 操作符 | 用于求两个集合的并集
+        vocabSet = vocabSet | set(document)  # union of the two sets
+    return list(vocabSet)
